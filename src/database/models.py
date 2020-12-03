4fd9c273ad6bd921d8462edf529379f8486819cc
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2020-12-02 21:55:15 -0600

    Implement Actor model

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,7 @@
 from settings import DB_PATH, TEST_DB_PATH
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from utils.gender import Gender
 
 
 db = SQLAlchemy()
@@ -32,4 +33,9 @@ class Movie(db.Model):
 
 
 class Actor(db.Model):
-    pass
+    __tablename__ = "actors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    age = db.Column(db.Datetime, nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
+    actors = db.relationship("Movie", secondary=actor_movie, backref="actors")
