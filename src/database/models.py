ce349bd800aaacfb6a13eb70918cb479da59d025
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2020-12-02 21:54:27 -0600

    implement association table for movies and actors

--- src/database/models.py
+++ src/database/models.py
@@ -16,6 +16,13 @@ def setup_db(app, test=False):
     db.create_all()
 
 
+actor_movie = db.Table(
+    "actor_movie",
+    db.Column("actor_id", db.Integer, db.ForeignKey("actors.id"), primary_key=True),
+    db.Column("movie_id", db.Integer, db.ForeignKey("movies.id"), primary_key=True),
+)
+
+
 class Movie(db.Model):
     pass
 
