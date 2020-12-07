6caf761877e49d21b2a8e76b3d9db594412e752f
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2020-12-07 15:27:51 -0600

    add validation for detach movie/actors

--- src/database/schemas.py
+++ src/database/schemas.py
@@ -8,19 +8,23 @@ class ActorSchema(Schema):
     DOB = fields.Date(required=True)
     gender = fields.Str(required=True, validate=validate.OneOf(
         ["male", "female", "other"]))
-    movies = fields.List(fields.String)
+    movies = fields.List(fields.Int)
+    detach_movies = fields.List(fields.Int)
 
 
 class MovieSchema(Schema):
     id = fields.Int()
     title = fields.Str(required=True, validate=validate.Length(min=1))
     release_date = fields.Date(required=True)
-    cast = fields.List(fields.String)
+    cast = fields.List(fields.Int)
+    detach_cast = fields.List(fields.Int)
 
 
 actor_schema = ActorSchema(unknown=EXCLUDE)
-actor_only_schema = ActorSchema(exclude=["movies"])
+actor_only_schema = ActorSchema(exclude=["movies", "detach_movies"])
+actor_schema_partial = ActorSchema(unknown=EXCLUDE, partial=True)
 
 
 movie_schema = MovieSchema(unknown=EXCLUDE)
-movie_only_schema = MovieSchema(exclude=["cast"])
+movie_only_schema = MovieSchema(exclude=["cast", "detach_cast"])
+movie_schema_partial = MovieSchema(unknown=EXCLUDE, partial=True)
