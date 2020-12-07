9e2c4144dce4faa0a657e12805e949f93d33df38
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2020-12-07 11:13:13 -0600

    implement post actor  unit test

--- src/test_api.py
+++ src/test_api.py
@@ -30,7 +30,66 @@ class CastingTestingCase(unittest.TestCase):
         self.assertFalse(data["success"])
         self.assertEqual(data["error"], 404)
 
+    def test_post_actors(self):
+        res = self.client.post(
+            "/actors", json={
+                "name": 'alejandro',
+                "DOB": '2002-03-11',
+                "gender": str(Gender.male.name)
+            })
+        data = res.get_json()
+
+        with self.app.app_context():
+            actor = Actor.query.filter_by(name="alejandro").first()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["id"], actor.id)
+
+    def test_post_actors_error(self):
+        res = self.client.post(
+            "/actors", json={
+                "name": 'alejandro',
+                "DOB": '2002-03-33',
+                "gender": Gender.male.name
+            })
+        data = res.get_json()
+        self.assertEqual(res.status_code, 422)
+        self.assertFalse(data["success"])
+
+        res1 = self.client.post(
+            "/actors", json={
+                "name": 'alejandro',
+                "DOB": '2002-03-30',
+                "gender": "not a gender"
+            })
+        data1 = res1.get_json()
+        self.assertEqual(res1.status_code, 422)
+        self.assertFalse(data1["success"])
+
+        res2 = self.client.post(
+            "/actors", json={
+                "name": '',
+                "DOB": '2002-03-30',
+                "gender": "not a gender"
+            })
+        data2 = res2.get_json()
+        self.assertEqual(res2.status_code, 422)
+        self.assertFalse(data2["success"])
+
+        res3 = self.client.post(
+            "/actors", json={
+                "name": 'alejandro',
+                "DOB": '2002-03-30',
+                "gender": Gender.male.name,
+                "movies": [30]
+            })
+        data3 = res3.get_json()
+        self.assertEqual(res3.status_code, 422)
+        self.assertFalse(data3["success"])
+
     # Movies test cases
+
     def test_get_movies(self):
         res = self.client.get("/movies")
         data = res.get_json()
